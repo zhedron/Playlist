fc5c1474aa17fa76b0c696b72ea6236bdf568f51
zhedron <zhedron@gmail.com>
2026-03-04 17:51:16 +0200

    fix

--- src/main/java/zhedron/playlist/entity/Playlist.java
+++ src/main/java/zhedron/playlist/entity/Playlist.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
